96af1a31b9ac2166f610579a2832bf393af51198
Mirko Contini <mirko.contini01@gmail.com>
2023-12-23 15:00:14 +0100

    .

--- Ecommerce/StoreManager/test/test_Unitari/testCarrelloProdotto.py
+++ Ecommerce/StoreManager/test/test_Unitari/testCarrelloProdotto.py
@@ -7,47 +7,39 @@ from StoreManager.models import Utente
 from StoreManager.models import Ordine
 
 
-
 class testCarrelloProdotto (unittest.TestCase):
 
     def setUp(self):
         # Creare un utente di esempio
-        #self.utente = Utente.objects.create(
-            #username = "giannimunari123", nome = "Gianni", cognome = "Munari", telefono = "333123456", email = "pippo@test.com", password = "pippo")
-
-        self.utente = Utente(username ="giannimunari123", nome="Gianni", cognome="Munari", telefono="33123456" email="pippo@test.com" password="pippo")
+        self.utente = Utente(username="giannimunari123", nome="Gianni", cognome="Munari",
+                             telefono="33123456", email="pippo@test.com", password="pippo")
         self.utente.save()
 
         # Creare un carrello associato all'utente
-        #self.carrello = Carrello.objects.create(utente=self.utente)
         self.carrello = Carrello(utente=self.utente)
         self.carrello.save()
 
-        # Creare un prodotto di esempio
-        #self.prodotto = Prodotto.objects.create(
-         #   nome_prodotto='Pasta', tipologia='Cibo', descrizione='Pasta Barilla', prezzo=2.0, quantita=10)
-        self.prodotto = Prodotto(nome_prodotto="Pasta", tipologia="Cibo", descrizione="Pasta Barilla", prezzo=2.0, quantita=10)
+        # Creare un prodotto di test
+        self.prodotto = Prodotto(nome_prodotto="Pasta", tipologia="Cibo",
+                                 descrizione="Pasta Barilla", prezzo=2.0, quantita=10)
         self.prodotto.save()
 
-        # Creare un ordine di esempio
-        #self.ordine = Ordine.objects.create(
-         #   utente=self.utente, data_ordine='2023-01-01')
+        # Creare un ordine di test
         self.ordine = Ordine(utente=self.utente, data_ordine="2023-01-01")
         self.ordine.save()
 
     def test_str(self):
-        # Creare un oggetto CarrelloProdotto di esempio
-        #carrello_prodotto = CarrelloProdotto.objects.create(
-        #    carrello=self.carrello, prodotto=self.prodotto, quantita=3, ordine=self.ordine)
-        self.carrello_prodotto = CarrelloProdotto(carrello=self.carrello, prodotto=self.prodotto, quantita=3, ordine=self.ordine)
+        self.carrello_prodotto = CarrelloProdotto(
+            carrello=self.carrello, prodotto=self.prodotto, quantita=3, ordine=self.ordine)
         self.carrello_prodotto.save()
 
         # Verificare che il metodo __str__ restituisca la stringa attesa
-        expected_str = f"{self.utente.username} - {self.prodotto.nome_prodotto} - 3"
+        expected_str = f"{
+            self.utente.username} - {self.prodotto.nome_prodotto} - 3"
         self.assertEqual(str(self.carrello_prodotto), expected_str)
 
     # Aggiungi altri metodi di test secondo necessità
-        
+
     def tearDown(self):
         # Elimina gli oggetti di test
         Utente.objects.all().delete()
